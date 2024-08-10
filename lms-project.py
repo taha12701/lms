92425091517cc524f7b0d4862e08779222ff2c89
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-08-10 23:19:38 +0500

    Add files via upload

---
+++ lms-project.py
@@ -0,0 +1,58 @@
+# class College:  #author
+#   message=".....Welcome to Uet......."
+
+#   def __init__(self,name='',roll=0,section='',unq=''):   #constructor values design
+#     self.name=name
+#     self.roll=roll
+#     self.section=section
+#     self._unq=unq
+
+#   @staticmethod
+#   def welcome_msg():
+#     print(College.message)
+    
+
+#   def get_input(self):            #taking input method
+#     self.name=input("Enter your name : ")
+#     self.roll=input("Enter your roll# : ")
+#     self.section=input("Enter your section : ")
+#     self._unq=input("Your unique id : ")
+#     self.dep=input("Your choosen departement : ")
+   
+
+   
+#   def input(self):                 #diplaying input method
+#     print(f"Student name = {self.name} ")
+#     print(f"Student roll# = {self.roll} ")
+#     print(f"Student section = {self.section} ")
+#     print(f"Student unq = {self._unq} ")
+#     print(f"Student department = {self.dep} ")
+    
+  
+#   def unique(self,unq):        #unique id method
+#     self._unq=unq
+
+ 
+#   def dep(self,dep):              #department methof
+#     self.dep=dep
+
+#   @staticmethod
+#   def mess():                     #last message method
+#     print(".....Thank you for Choosen our College.....")
+
+
+
+
+
+
+# #class methods are calling    
+# c1=College()
+# c1.get_input()
+# c1.welcome_msg()
+# c1.input()
+# c1.mess()
+                                   
+
+
+
+
